0cc59e82b80a8c257ad6dd466c17b432b814b38e
Alexandre <alexandre.laverre@unil.ch>
2023-02-09 18:48:58 +0100

    Integration of substitution matrix per chrom and optimise execution time

--- detect_positive_selection/testPosSelec.py
+++ detect_positive_selection/testPosSelec.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import sys
+import os
 from Bio import SeqIO
 from Bio.Seq import Seq
 import numpy as np
+import pandas
 from alive_progress import alive_bar
 
-path = "/Users/alaverre/Documents/Detecting_positive_selection/results/human/CEBPA/"
-Ancestral_fasta = path + "filtered_ancestral_sequences.fa_subsample"
-Focal_fasta = path + "filtered_focal_sequences.fa_subsample"
-ModelEstimation = path + "Model/kmer_predicted_weight.txt"
-Substitution_file = path  # unused for the moment
-NbRand = 1000             # number of random substitutions permutations per sequence
+species = sys.argv[1]       # Species name: human dog
+NbRand = int(sys.argv[2])        # Number of random substitutions permutations per sequence
+Evol = sys.argv[3]          # Substitution model: uniform or proba
 
-Output = open(path + "PosSelTest_deltaSVM_" + str(NbRand) + "permutations.txt_subsample", "w")
+path = "/Users/alaverre/Documents/Detecting_positive_selection/results/"
+pathSelection = path + "positive_selection/" + species + "/CEBPA/"
+Ancestral_fasta = pathSelection + "filtered_ancestral_sequences.fa_subsample"
+Focal_fasta = pathSelection + "filtered_focal_sequences.fa_subsample"
+ModelEstimation = pathSelection + "Model/kmer_predicted_weight.txt"
+pathSubMat = path + "/substitution_matrix/dog/"
+
+Output = open(pathSelection + "PosSelTest_deltaSVM_" + str(NbRand) + "permutations.txt_subsample_new", "w")
 
 
 ####################################################################################################
@@ -35,26 +42,27 @@ def calculate_delta_svm(seq_ref, seq_alt, kmer_len, svm_scores):
 
 
 # Get random sequences according to substitution matrix
-def get_random_seqs(seq, sub_prob, sub_prob_norm, sub_dir, nb_sub, nb_perm):
+def get_random_seqs(seq, sub_prob, sub_prob_norm, nb_sub, nb_perm):
     # Get substitution probabilities for each position
     pos_proba = np.empty(len(seq))
     for pos in range(len(seq)):
-        nucl = seq[pos]                             # the probability to draw a given position is equal to the sum
-        pos_proba[pos] = sum(sub_prob[nucl])        # of the substitution probabilities from this nucleotide
+        nucl = seq[pos]                                # the probability to draw a given position is equal to the sum
+        pos_proba[pos] = sum(sub_prob[nucl].values())  # of the substitution probabilities from this nucleotide
 
     normed_pos_proba = pos_proba / sum(pos_proba)   # normalisation of all positions to sum at 1
 
     random_seqs = []
     for perm in range(nb_perm):
         rand_seq = seq
-
         # draw positions
         rand_pos = np.random.choice(np.arange(normed_pos_proba.size), p=normed_pos_proba, replace=False, size=nb_sub)
 
         # draw directions
         for pos in rand_pos:
             old_nuc = seq[pos]
-            new_nuc = np.random.choice(sub_dir[old_nuc], p=sub_prob_norm[old_nuc])
+            directions = list(sub_prob_norm[old_nuc].keys())
+            proba = list(sub_prob_norm[old_nuc].values())
+            new_nuc = np.random.choice(directions, p=proba)
             rand_seq = rand_seq[:pos] + new_nuc + rand_seq[pos+1:]  # change value in pos
 
         random_seqs.append(rand_seq)
@@ -63,30 +71,47 @@ def get_random_seqs(seq, sub_prob, sub_prob_norm, sub_dir, nb_sub, nb_perm):
 
 
 ####################################################################################################
+# Binding affinity values per kmer
 print("Get SVM score for each kmer from:", ModelEstimation)
 SVM_dict = {}
 with open(ModelEstimation, 'r') as model:
     for i in model.readlines():
         i = i.strip("\n")
         i = i.split("\t")
-        kmer = Seq(i[0])
+        kmer = i[0]
         KmerLen = len(kmer)
         svm_score = float(i[1])
-        rev_kmer = kmer.reverse_complement()  # add the reverse complement kmer
+        rev_kmer = str(Seq(kmer).reverse_complement())  # add the reverse complement kmer
 
         SVM_dict[kmer] = svm_score
         SVM_dict[rev_kmer] = svm_score
 
-# print("Get substitutions matrix from:", Substitution_file)
-# SubMat = open(Substitution_file)
-# Temporary uniform substitution matrix
-SubMat_direction = {'A': ['T', 'C', 'G'], 'T': ['A', 'C', 'G'], 'C': ['A', 'T', 'G'], 'G': ['A', 'T', 'C']}
-SubMat_proba = {'A': [0.33, 0.33, 0.33], 'T': [0.33, 0.33, 0.33], 'C': [0.33, 0.33, 0.33], 'G': [0.33, 0.33, 0.33]}
 
-# Normalisation of probabilities to sum at 1 for each nucleotide --> useless if uniform
-SubMat_proba_normed = {}
-for nuc in SubMat_proba.keys():
-    SubMat_proba_normed[nuc] = [sub_proba / sum(SubMat_proba[nuc]) for sub_proba in SubMat_proba[nuc]]
+# Substitution matrix
+if Evol == 'uniform':
+    SubMat_uniform = {'A': {'A': 0, 'C': 0.333, 'G': 0.333, 'T': 0.334},
+                      'C': {'A': 0.333, 'C': 0, 'G': 0.333, 'T': 0.334},
+                      'G': {'A': 0.333, 'C': 0.333, 'G': 0, 'T': 0.334},
+                      'T': {'A': 0.333, 'C': 0.333, 'G': 0.334, 'T': 0}}
+else:
+    print("Get substitution matrix for each chromosome...")
+    SubMats = {}
+    SubMats_norm = {}
+    for file in os.listdir(pathSubMat):
+        if file.endswith('.txt'):
+            chrom = 'chr' + file.strip('.txt')
+
+            chrom_Table = pandas.read_table(pathSubMat + file, sep=' ')
+            chrom_Table.index = ['A', 'C', 'G', 'T']     # change row values
+            np.fill_diagonal(chrom_Table.values, 0)      # assign 0 to diagonal
+            chrom_SubMat = chrom_Table.to_dict('index')  # get dict by matrix rows
+            SubMats[chrom] = chrom_SubMat
+
+            # Get substitution probabilities of each nucleotide to sum at 1 for drawing directions
+            chrom_Table_norm = chrom_Table.div(chrom_Table.sum(axis=1), axis=0)
+            chrom_SubMat_norm = chrom_Table_norm.to_dict('index')
+            SubMats_norm[chrom] = chrom_SubMat_norm
+
 
 # Sequences
 print("Get Ancestral sequences...")
@@ -102,15 +127,20 @@ print("Running Positive Selection test for each sequence with", NbRand, "random 
 with alive_bar(len(FocalSeqs.keys())) as bar:
     for ID in FocalSeqs.keys():
         bar()   # Progress bar
-        FocalSeq = FocalSeqs[ID].seq
-        AncestralSeq = AncestralSeqs[ID].seq
+        FocalSeq = str(FocalSeqs[ID].seq)
+        AncestralSeq = str(AncestralSeqs[ID].seq)
 
+        # Get corresponding substitution matrix
+        chrom = ID.split(':')[0]
+        SubMat_proba = SubMats[chrom] if Evol != 'uniform' else SubMat_uniform
+        SubMat_proba_normed = SubMats_norm[chrom] if Evol != 'uniform' else SubMat_uniform
+
+        # Number of substitutions between Ancestral and Focal sequences
         NbSub = get_sub_number(FocalSeq, AncestralSeq)
-
         if NbSub > 1:
-            DeltaObs = calculate_delta_svm(AncestralSeq, FocalSeq, KmerLen, SVM_dict)
 
-            RandomSeqs = get_random_seqs(AncestralSeq, SubMat_proba, SubMat_proba_normed, SubMat_direction, NbSub, NbRand)
+            DeltaObs = calculate_delta_svm(AncestralSeq, FocalSeq, KmerLen, SVM_dict)
+            RandomSeqs = get_random_seqs(AncestralSeq, SubMat_proba, SubMat_proba_normed, NbSub, NbRand)
             DeltaRand = [calculate_delta_svm(AncestralSeq, RandSeq, KmerLen, SVM_dict) for RandSeq in RandomSeqs]
 
             # Calculate p-value
