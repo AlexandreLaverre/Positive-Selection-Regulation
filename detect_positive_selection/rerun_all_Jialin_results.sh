318f22c3716365b9e9a9e59254ce158d97d5586e
Alexandre <alexandre.laverre@unil.ch>
2023-02-10 15:24:37 +0100

    first commit

---
+++ detect_positive_selection/rerun_all_Jialin_results.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sp=$1
+
+path="/Users/alaverre/Documents/Detecting_positive_selection/"
+pathData="${path}/Tools/JialinTool/data/${sp}/"
+pathResults="${path}/results/positive_selection/rerun_Jialin_corrected/${sp}/"
+
+mkdir -p ${pathResults}
+
+if [ ${sp} = "human" ]; then
+	samples=("CEBPA" "HNF4A")
+	conditions=()
+	
+elif [ ${sp} = "mouse" ]; then
+	samples=("CEBPA" "HNF4A" "FOXA1")
+	conditions=("specific_loss_" "specific_gain_" "conserved_")
+	
+elif [ ${sp} = "fly" ]; then
+	samples=("CTCF")
+	conditions=("specific_loss_" "specific_gain_" "conserved_")
+fi
+
+####################################################################################
+
+for samp in "${samples[@]}"
+do
+	echo ${samp}
+	for cond in "${conditions[@]}"
+	do
+		echo ${cond}
+		./testPosSelec.pl ${pathData}/${sp}_sequences/${samp}_${cond}filtered_ancestor.fa ${pathData}/${sp}_sequences/${samp}_${cond}filtered_focal.fa ${pathData}/${sp}_SVM_model/${samp}/kmer_10_library_weigths.txt 10000 ${pathResults}/${samp}_deltaSVM.txt
+	done
+done
+
+
+####################################################################################
