d4ebde709b467c65cd9450440d4f36fec2dd0cca
Alexandre <alexandre.laverre@unil.ch>
2023-03-01 16:47:39 +0100

    Check if coordinates are named according to Ensembl or UCSC

--- detect_positive_selection/GenerateNegativeSeq.R
+++ detect_positive_selection/GenerateNegativeSeq.R
@@ -1,27 +1,32 @@
 #!/usr/bin/env Rscript
-library(gkmSVM)
-library(BSgenome)
-library(BiocManager)
+suppressMessages(library(gkmSVM))
+suppressMessages(library(BSgenome))
+suppressMessages(library(BiocManager))
+#library(reticulate)
+#use_python("/Users/alaverre/miniconda3/bin/python")
 args = commandArgs(trailingOnly=TRUE)
 
 species = args[1] #"human"
 sample = args[2]  #"CEBPA"
 BED_file = args[3]
 pathResults = args[4]
+pathScripts = "/Users/alaverre/Documents/Detecting_positive_selection/scripts/compare_genome_assemblies"
 
-#species = "human"
-#sample = "CEBPA"
-#BED_file = "/Users/alaverre/Documents/Detecting_positive_selection/Tools/JialinTool/data/human/human_ChIP-Seq/hsap_CEBPA.bed2"
-#pathResults = "/Users/alaverre/Documents/Detecting_positive_selection/results/tools_tests/human/CEBPA/Test-models/run_15/"
+# Check if chrName conversion between Ensembl and UCSC is needed
+BED = read.table(BED_file)
+if ("Ensembl" %in% seqlevelsStyle(BED$V1)){
+  # Run chromosome names correspondence
+  system(paste0("python ", pathScripts, "/convert.BED.chrNames.py ", species, " ", BED_file))
+  BED_file = paste0(BED_file, "_UCSC_names")
+}
 
-if (species == "mouse"){
-  genome <- getBSgenome("BSgenome.Mmusculus.UCSC.mm9.masked")
-}else{
-  genome <- getBSgenome("BSgenome.Hsapiens.UCSC.hg38.masked")}
+if (species == "mouse"){genome <- getBSgenome("BSgenome.Mmusculus.UCSC.mm9.masked")}
+if (species == "human"){genome <- getBSgenome("BSgenome.Hsapiens.UCSC.hg38.masked")}
+if (species == "dog"){genome <- getBSgenome("BSgenome.Cfamiliaris.UCSC.canFam3.masked")}
 
 # For new genomes
 #available.genomes()
-#install("BSgenome.Mmusculus.UCSC.mm10.masked")
+#BiocManager::install("BSgenome.Mmusculus.UCSC.mm10.masked")
 
 ###############################################################################
 # Generate negative sequences
