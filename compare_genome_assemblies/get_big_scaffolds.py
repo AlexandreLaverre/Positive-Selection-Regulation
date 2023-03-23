f70072507f15c3a459f3ccda5eb69261ca8f40d3
Alexandre <alexandre.laverre@unil.ch>
2023-03-23 12:11:27 +0100

    add prints and gzip the output

--- compare_genome_assemblies/get_big_scaffolds.py
+++ compare_genome_assemblies/get_big_scaffolds.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # coding=utf-8
 import sys
+import gzip
 from Bio import SeqIO
 
-Genome = sys.argv[1]
+Genome = gzip.open(sys.argv[1], 'rt')
 minLength = int(sys.argv[2])
-Output = sys.argv[3]
+Output = gzip.open("sup2kb" + sys.argv[1], 'wt')
 
 N = 0
 big_scaffolds = []
 
 print("Reading genome...")
-for scaffold in SeqIO.parse(open(Genome), "fasta"):
+for scaffold in SeqIO.parse(Genome, "fasta"):
     N += 1
     if len(scaffold.seq) >= minLength:
         big_scaffolds.append(scaffold)
@@ -19,3 +20,6 @@ for scaffold in SeqIO.parse(open(Genome), "fasta"):
 print("Found", len(big_scaffolds), "scaffolds larger than", minLength, "bp within a total of", N)
 print("Writing output...")
 SeqIO.write(big_scaffolds, Output, "fasta")
+
+Genome.close()
+Output.close()
