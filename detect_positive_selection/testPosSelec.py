6ae2491a677e4771aa021c12a4c4733dbac34327
Alexandre <alexandre.laverre@unil.ch>
2023-04-03 15:26:06 +0200

    increase TestPos max time limit

--- detect_positive_selection/testPosSelec.py
+++ detect_positive_selection/testPosSelec.py
@@ -181,6 +181,7 @@ if __name__ == '__main__':
             for result in pool.imap_unordered(test_positive_selection, SeqIDs):
                 bar()  # print progress bar
                 if result is not None:
+                    print(result)
                     Output.write(result)
 
 Output.close()
