b343634c82e9c1b9b280b952f31f2ff53719f70f
alaverre <alexandre.laverre@unil.ch>
2023-08-07 14:53:21 +0200

    add dryRun option

--- detect_positive_selection/run_Snakemake.sh
+++ detect_positive_selection/run_Snakemake.sh
@@ -5,10 +5,15 @@ export sample=$2				# i.e: CEBPA HNF4A ...
 export cluster=$3				# i.e: local or cluster
 export nbThreads=$4				# i.e: int (number of part for parallelization)
 export nbRand=$5				# i.e: int (randomization)
-export alignType=${6:-"MAF"}			# i.e: MAF or pairwise
-export method=${7:-"parsimony"}			# i.e: only working with alignType=pairwise
+export dryRun=${6:-""}				# i.e: -n or nothing (run snakemake in dry-run mode"
+export alignType=${7:-"MAF"}			# i.e: MAF or pairwise
+export method=${8:-"parsimony"}			# i.e: only working with alignType=pairwise
 
 export Prefix=${species}_${sample}
+
+if [ ${dryRun} = "-n" ]; then
+	echo "Run Snakemake in dry-run mode"
+fi
 
 ##################################################################
 
@@ -24,6 +29,6 @@ conda activate TestPos
 
 ##################################################################
 
-snakemake --rerun-triggers mtime -j 50 --config sp=${species} sample=${sample} AlignType=${alignType} AncMethod=${method} cluster=${cluster} nbPart=${nbThreads} nbRand=${nbRand} --rerun-incomplete --cluster "sbatch -p cpu -N 1 -o ${pathLog}/slurm.out_${Prefix} -e ${pathLog}/slurm.err_${Prefix} -c {params.threads} --mem={params.mem} -t {params.time}"
+snakemake ${dryRun} --rerun-triggers input -j 50 --config sp=${species} sample=${sample} AlignType=${alignType} AncMethod=${method} cluster=${cluster} nbPart=${nbThreads} nbRand=${nbRand} --rerun-incomplete --cluster "sbatch -p cpu -N 1 -o ${pathLog}/slurm.out_${Prefix} -e ${pathLog}/slurm.err_${Prefix} -c {params.threads} --mem={params.mem} -t {params.time}"
 
 ##################################################################
