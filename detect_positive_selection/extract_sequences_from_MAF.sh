03d985d88c2f1694c84e14476bab442192fb1505
Alexandre <alexandre.laverre@unil.ch>
2023-10-18 11:21:16 +0200

    add sequences extraction of the sister specie

--- detect_positive_selection/extract_sequences_from_MAF.sh
+++ detect_positive_selection/extract_sequences_from_MAF.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sp=$1    		# i.e: human or mouse
-sample=$2    	# i.e: CEBPA or HNF4A
-TF=$3
+sp=$1    		  # e.g: human
+sample=$2     # e.g: Wilson
+TF=$3	        # e.g: CEBPA
 BED_file=$4
-cluster=$5
+cluster=$5    # e.g: local
 
 #########################################################################
 # Define paths
 
-if [ ${cluster} = "local" ]; then
+if [ "${cluster}" = "local" ]; then
 	export path=/Users/alaverre/Documents/Detecting_positive_selection
 	source /Users/alaverre/miniconda3/etc/profile.d/conda.sh
 else
@@ -30,51 +30,55 @@ fi
 
 mkdir -p "${pathAlign}"
 mkdir -p "${pathResults}/focal_sequences/"
+mkdir -p "${pathResults}/sister_sequences/"
 mkdir -p "${pathResults}/ancestral_sequences/"
 
 # Define focal and ancestral names
-source ${path}/scripts/params.sh ${sp} ${cluster}
+# shellcheck disable=SC2086
+source ${path}/scripts/params.sh "$sp" "$cluster"
+sister_name="$(echo "$close_species" | cut -d "," -f 2)"
 
 #########################################################################
 # Retrieve alignments of regions from whole genome alignment
-mafsInRegion ${BED_file} -outDir ${pathAlign}/ ${pathGenomeAlign}
+mafsInRegion "$BED_file" -outDir "${pathAlign}"/ "$pathGenomeAlign"
 
 # Retrieve ancestral and focal sequences
-for ID in `cat "${BED_file}" | cut -f 4`
+for ID in `cat "$BED_file" | cut -f 4`
 do
 	#ID=$(echo ${line} | cut -f 4 -d ' ')
 	#echo "${ID}"
 	align="${pathAlign}"/"${ID}".maf
 
 	# Convert to FASTA
-	msa_view --missing-as-indels -i MAF -o FASTA "${align}" > "${pathAlign}"/"${ID}".mfa
+	msa_view --missing-as-indels -i MAF -o FASTA "$align" > "${pathAlign}"/"${ID}".mfa
 	sed -i 's/ //g' "${pathAlign}"/"${ID}".mfa
 
   # Check if an alignment exists with the other species
 	if [ -s "${pathAlign}"/"${ID}".mfa ]; then
 	
 	    # Keep only focal and ancestral sequences
-	    seqkit grep -p ${sp_name} -p "${anc_name}" "${pathAlign}"/"${ID}".mfa > "${pathAlign}"/"${ID}"_anc_foc.mfa
+	    #seqkit grep -p ${sp_name} -p "${anc_name}" "${pathAlign}"/"${ID}".mfa > "${pathAlign}"/"${ID}"_anc_foc.mfa
 
 	    # Remove GAP
-	    trimal -nogaps -keepheader -in "${pathAlign}"/"${ID}"_anc_foc.mfa -out "${pathAlign}"/"${ID}"_anc_foc_nogap.mfa
-	    rm ${pathAlign}/"${ID}"_anc_foc.mfa "${pathAlign}"/"${ID}".mfa
+	    trimal -nogaps -keepheader -in "${pathAlign}"/"${ID}".mfa -out "${pathAlign}"/"${ID}"_anc_foc_nogap.mfa
+	    rm "${pathAlign}"/"${ID}"_anc_foc.mfa "${pathAlign}"/"${ID}".mfa
 
 	    # Check if alignment still exists after removing GAPs
-	    if [ -f ${pathAlign}/"${ID}"_anc_foc_nogap.mfa ]; then
-		    # Get focal and ancestral sequences separatly
-		    seqkit grep -p ${sp_name} ${pathAlign}/"${ID}"_anc_foc_nogap.mfa > ${pathResults}/focal_sequences/"${ID}"_nogap.fa
-		    seqkit grep -p ${anc_name} ${pathAlign}/"${ID}"_anc_foc_nogap.mfa > ${pathResults}/ancestral_sequences/"${ID}"_nogap.fa
+	    if [ -f "${pathAlign}"/"${ID}"_anc_foc_nogap.mfa ]; then
+		    # Get focal and ancestral sequences separately
+		    seqkit grep -p "$sp_name" "${pathAlign}"/"${ID}"_anc_foc_nogap.mfa > "${pathResults}"/focal_sequences/"${ID}"_nogap.fa
+		    seqkit grep -p "$sister_name" "${pathAlign}"/"${ID}"_anc_foc_nogap.mfa > "${pathResults}"/sister_sequences/"${ID}"_nogap.fa
+        seqkit grep -p "$anc_name" "${pathAlign}"/"${ID}"_anc_foc_nogap.mfa > "${pathResults}"/ancestral_sequences/"${ID}"_nogap.fa
 
 		    # Change species names to ID
-		    sed -i "s/${sp_name}/"${ID}"/g" ${pathResults}/focal_sequences/"${ID}"_nogap.fa
-		    sed -i "s/${anc_name}/"${ID}"/g" ${pathResults}/ancestral_sequences/"${ID}"_nogap.fa
+		    sed -i "s/${sp_name}/${ID}/g" "${pathResults}"/focal_sequences/"${ID}"_nogap.fa
+		    sed -i "s/${anc_name}/${ID}/g" "${pathResults}"/ancestral_sequences/"${ID}"_nogap.fa
 	    else
-	    	echo "${ID}" >> ${pathResults}/missing.txt
+	    	echo "${ID}" >> "${pathResults}"/missing.txt
 	    fi
 	    
   	else
-    		echo "${ID}" >> ${pathResults}/missing.txt
+    		echo "${ID}" >> "${pathResults}"/missing.txt
   	fi
   
 done
