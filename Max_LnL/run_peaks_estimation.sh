65ea521d78e7b6332f839ede214c6a0b424887cb
alaverre <alexandre.laverre@unil.ch>
2023-11-30 16:33:46 +0100

    change time for 500 peaks per hour per cpu

--- Max_LnL/run_peaks_estimation.sh
+++ Max_LnL/run_peaks_estimation.sh
@@ -14,8 +14,8 @@ if [ ${cluster} = "cluster" ]; then
 	export path=/work/FAC/FBM/DEE/mrobinso/evolseq/DetectPosSel/
 	export pathScripts=/work/FAC/FBM/DEE/mrobinso/evolseq/DetectPosSel/scripts/Max_LnL/
 
-	# Calculate time needed: considering 1000 peaks per hour and per thread
-	export time=$(( ($(wc -l < ${path}/results/positive_selection/all_deltas/${species}/${sample}/observed_deltaSVM.txt) / (1000*${nbThread}))+1 ))
+	# Calculate time needed: considering 500 peaks per hour and per thread
+	export time=$(( ($(wc -l < ${path}/results/positive_selection/all_deltas/${species}/${sample}/observed_deltaSVM.txt) / (500*${nbThread}))+1 ))
 
 	# BATCH informations
 	echo "#!/bin/sh" > ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
@@ -23,7 +23,7 @@ if [ ${cluster} = "cluster" ]; then
 	echo "#SBATCH --output=${pathScripts}/logs/std_output_MaxLL_${Prefix}.txt" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 	echo "#SBATCH --error=${pathScripts}/logs/std_error_MaxLL_${Prefix}.txt" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 	echo "#SBATCH --partition=cpu" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
-	echo "#SBATCH --mem=4G" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
+	echo "#SBATCH --mem=16G" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 	echo "#SBATCH --cpus-per-task=${nbThread}" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 	echo "#SBATCH --time=${time}:00:00" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 
