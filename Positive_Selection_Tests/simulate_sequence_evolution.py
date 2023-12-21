08e8289ca0f8c59ef39bf44c47653c671c50ddc2
Alexandre <alexandre.laverre@unil.ch>
2023-12-21 18:48:08 +0100

    change strategy to simulate with 500 random seq

--- Positive_Selection_Tests/simulate_sequence_evolution.py
+++ Positive_Selection_Tests/simulate_sequence_evolution.py
@@ -4,6 +4,7 @@ import numpy as np
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+import random
 from multiprocessing import Pool
 from alive_progress import alive_bar
 import sys
@@ -22,35 +23,30 @@ NbThread = 8
 
 ####################################################################################################
 def get_simulated_sequences(seq_id):
-    seq = str(initial_sequences[seq_id].seq)
-    chromosome = seq_id.split(':')[0]
-    sub_mat_proba = SubMats[chromosome]
-    sub_mat_proba_norm = SubMats_norm[chromosome]
-    nsub = np.random.randint(2, max_mut)
+    original_seq = str(initial_sequences[seq_id].seq)
+    if 20 <= len(original_seq) <= 1000:
+        chromosome = seq_id.split(':')[0]
+        sub_mat_proba = SubMats[chromosome]
+        sub_mat_proba_norm = SubMats_norm[chromosome]
+        nsub = np.random.randint(2, max_mut)
 
-    if 20 <= len(seq) <= 1000:
-        null_seq = pos_seq = stab_seq = None
-        attempt = 0
-        while any(model is None for model in (null_seq, pos_seq, stab_seq)):
-            random_seq = SVM.get_random_seqs(seq, sub_mat_proba, sub_mat_proba_norm, n_sub=nsub, n_rand=1)
-            delta = SVM.calculate_delta(seq, random_seq, SVM_dict)
+        # Simulate 500 sequences
+        random_seq = SVM.get_random_seqs(original_seq, sub_mat_proba, sub_mat_proba_norm, n_sub=nsub, n_rand=500)
+        deltas = {}
+        for seq in random_seq:
+            deltas[seq] = SVM.calculate_delta(original_seq, seq, SVM_dict)
 
-             # Low delta = Stabilising selection
-            if -0.5 < delta < 0.5 and stab_seq is None:
-                stab_seq = SeqRecord(Seq(random_seq), id=seq_id, description="")
+        # Stabilising selection: find the lowest change in delta
+        min_seq = min(deltas, key=lambda k: abs(deltas[k]))
+        stab_seq = SeqRecord(Seq(min_seq), id=seq_id, description="")
 
-            # High shift in delta = Positive selection
-            elif delta > 5 and pos_seq is None:
-                pos_seq = SeqRecord(Seq(random_seq), id=seq_id, description="")
+        # Positive selection: find the highest change in delta =
+        max_seq = max(deltas, key=lambda k: abs(deltas[k]))
+        pos_seq = SeqRecord(Seq(max_seq), id=seq_id, description="")
 
-            # Other = Random drift
-            elif null_seq is None:
-                null_seq = SeqRecord(Seq(random_seq), id=seq_id, description="")
-
-            attempt += 1
-            if attempt > 1000:
-                print(seq_id, "is highly constrained: skipped!")
-                break
+        # Random drift: random seq
+        rand_seq = random.choice(list(deltas.keys()))
+        null_seq = SeqRecord(Seq(rand_seq), id=seq_id, description="")
 
         return seq_id, stab_seq, pos_seq, null_seq
 
@@ -93,7 +89,7 @@ if __name__ == '__main__':
     dictionaries = {'stabilising': Stabilised_dict, 'positive': Positive_dict, 'neutral': Neutral_dict}
 
     for dict_name, dic in dictionaries.items():
-        with open(f"{PathSequence}/simulated_sequences_{dict_name}_evolution.fa", 'w') as output:
+        with open(f"{PathSequence}/simulated_sequences_{dict_name}_evolution_500.fa", 'w') as output:
             SeqIO.write(dic.values(), output, 'fasta')
 
 ####################################################################################################
