c3be1ea84e3abf053790cb600287c8f5f43e4e26
Alexandre <alexandre.laverre@unil.ch>
2024-02-02 11:01:24 +0100

    change input data

--- ChIPseq_analyses/define_mice_shared_peaks.R
+++ ChIPseq_analyses/define_mice_shared_peaks.R
@@ -5,7 +5,7 @@ library(ggvenn)
 
 options("stringsAsFactors"=FALSE)
 
-path <- "/Users/alaverre/Documents/Detecting_positive_selection/results/peaks_overlap/"
+path <- "/Users/alaverre/Documents/Detecting_positive_selection/results/"
 TF <- "CEBPA"
 species <- c("Mmusculus", "Mspretus", "Mcaroli")
 
@@ -13,20 +13,20 @@ species <- c("Mmusculus", "Mspretus", "Mcaroli")
 peaks <- list()
 lifted_peaks <- list()
 for (sp in species){
-  BED <- read.table(paste0(path, "peaks/", sp, "_", TF, ".peaks.bed"))
+  BED <- read.table(paste0(path, "peaks_overlap/peaks_by_type/", sp, "_", TF, ".peaks.bed"))
   colnames(BED) <- c("chr", "start", "end", "complete_ID")
   row.names(BED) <- paste0(BED$chr, ":", BED$start, ":", BED$end)
   BED[,sp] <- row.names(BED)
   
   for (target in setdiff(species, sp)){
     # Correspondence lifted
-    lifted <- read.table(paste0(path, "/lifted_peaks/", sp, "_", TF, "_to_", target, "_0.9.bed"), header=F)
+    lifted <- read.table(paste0(path, "peaks_overlap/lifted_peaks/", sp, "_", TF, "_to_", target, "_0.9.bed"), header=F)
     lifted[[target]] <- paste0(lifted$V1, ":", lifted$V2, ":", lifted$V3)
     row.names(lifted) <- lifted$V4
     lifted_peaks[[sp]][[target]] <- lifted
     
     # Overlapping peaks
-    overlap <- read.table(paste0(path, "/", TF, "_", sp, "_lifted_overlap_", target, ".txt"), header=T)
+    overlap <- read.table(paste0(path, "peaks_overlap/", TF, "_", sp, "_lifted_overlap_", target, ".txt"), header=T)
     BED[overlap$ID, target] <- overlap$overlap_ID
   }
   
@@ -53,24 +53,32 @@ sets_list <- list(Mmusculus = data_bool[which(data_bool$Mmusculus), "Nb"],
                   Mcaroli = data_bool[which(data_bool$Mcaroli), "Nb"],
                   Mspretus = data_bool[which(data_bool$Mspretus), "Nb"])
 
-#venn.plot <- venn.diagram(sets_list, filename = NULL)
-#grid.draw(venn.plot)
-#fit <- euler(data_bool)
-#plot(fit)
-
+pdf(file=paste0(path, "figures/mice_peaks_overlap.pdf"))
 ggvenn(sets_list)
+dev.off()
+
+ID_to_BED <- function(x){
+  data <- na.omit(as.data.frame(unique(x)))
+  colnames(data) <- "ID"
+  data$chr <- sapply(strsplit(data$ID,":"), `[`, 1)
+  data$start <- sapply(strsplit(data$ID,":"), `[`, 2)
+  data$end <- sapply(strsplit(data$ID,":"), `[`, 3)
+  data <- data[,c("chr", "start", "end", "ID")]
+}
 
 ################################################################################
 # Write output
 for (sp in species){
   conserved_row <- row.names(data_bool[which(data_bool$Mmusculus == TRUE & data_bool$Mcaroli == TRUE & data_bool$Mspretus == TRUE),])
-  conserved <- data[conserved_row, sp]
-  
+  conserved <- ID_to_BED(unlist(strsplit(data[conserved_row, sp],",")))
+
   other_sp1 = setdiff(species, sp)[1]
   other_sp2 = setdiff(species, sp)[2]
   gain_row <- row.names(data_bool[which(data_bool[[sp]] == TRUE & 
                                           data_bool[[other_sp1]] == FALSE &
                                           data_bool[[other_sp2]] == FALSE),])
+  
+  gain <- ID_to_BED(unlist(strsplit(data[gain_row, sp],",")))
   
   loss_row <- row.names(data_bool[which(data_bool[[sp]] == FALSE &
                                           data_bool[[other_sp1]] == TRUE &
@@ -78,11 +86,11 @@ for (sp in species){
   
   # Get the correspondence IDs from other_sp to sp
   loss_IDs <- data[loss_row, other_sp1]
-  loss_in_sp <- lifted_peaks[[other_sp1]][[sp]][loss_IDs, sp]
+  loss_in_sp <- ID_to_BED(lifted_peaks[[other_sp1]][[sp]][loss_IDs, sp])
   
-  write.table(data[conserved_row, sp], file = paste0(path, "/peaks/", sp, "_conserved_peaks.txt"), quote=F, col.names = F, row.names = F)
-  write.table(data[gain_row, sp], file = paste0(path, "/peaks/", sp, "_gain_peaks.txt"), quote=F, col.names = F, row.names = F)
-  write.table(loss_in_sp, file = paste0(path, "/peaks/", sp, "_loss_peaks.txt"), quote=F, col.names = F, row.names = F)
+  write.table(conserved, file = paste0(path, "peaks_overlap/peaks_by_type/", sp, "_conserved_peaks.txt"), quote=F, col.names = F, row.names = F, sep="\t")
+  write.table(gain, file = paste0(path, "peaks_overlap/peaks_by_type/", sp, "_gain_peaks.txt"), quote=F, col.names = F, row.names = F, sep="\t")
+  write.table(loss_in_sp, file = paste0(path, "peaks_overlap/peaks_by_type/", sp, "_loss_peaks.txt"), quote=F, col.names = F, row.names = F, sep="\t")
 }
 
 
