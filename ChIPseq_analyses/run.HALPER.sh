d89b18b765d7c9909f0b58c72b267dc53ca3f682
Alexandre <alexandre.laverre@unil.ch>
2024-03-21 11:54:41 +0100

    change to default params: from max_frac to max_len

--- ChIPseq_analyses/run.HALPER.sh
+++ ChIPseq_analyses/run.HALPER.sh
@@ -10,6 +10,7 @@ path=/beegfs/data/alaverre/PosSel/
 source ${path}/scripts/config/params.sh "${sp}" "${cluster}"
 pathData=${path}/results/peaks_calling/${sp}/
 pathResults=${path}/results/homologous_peaks/${sp}/${TF}
+pathLogs=${path}/scripts/ChIPseq_analyses/logs
 pathHAL=/beegfs/banque/hal/241-mammalian-2020v2.hal
 
 all_species=("Homo_sapiens" "Macaca_mulatta" "Mus_musculus" "Mus_spretus" "Mus_caroli" "Felis_catus" \
@@ -25,15 +26,17 @@ for target in "${other_species[@]}"; do
   else
   ######################################################################################################################
   # Write instructions for cluster
-  mkdir -p "${pathResults}"
-  mkdir -p "${path}/scripts/ChIPseq_analyses/logs/"
-  logFile=${path}/scripts/ChIPseq_analyses/logs/bsub_HALPER_"${sp}"_"${TF}"_to_"${target}"
+  mkdir -p "${pathResults}/liftover"
+  mkdir -p "${pathLogs}"
+  logFile=${pathLogs}/bsub_HALPER_"${sp}"_"${TF}"_to_"${target}"
 
   echo "#!/bin/bash" > "${logFile}"
   if [ "${cluster}" = "cluster" ]; then
     {
     echo "#SBATCH --job-name=HALPER_${TF}_${sp}_to_${target}"
     echo "#SBATCH --partition=normal"
+    echo "#SBATCH --output=${pathLogs}/std_output_${sp}_${TF}_to_${target}"
+    echo "#SBATCH --error=${pathLogs}/std_error_${sp}_${TF}_to_${target}"
     echo "#SBATCH --mem=5G"
     echo "#SBATCH --cpus-per-task=2"
     echo "#SBATCH --time=8:00:00"
@@ -45,16 +48,18 @@ for target in "${other_species[@]}"; do
   {
     # Lift summits
     echo "halLiftover --bedType 4 ${pathHAL} ${sp} ${pathData}/${TF}.consensus_summits.bed ${target} \
-          ${pathResults}/${TF}.consensus_summits_to_${target}.bed &"
+          ${pathResults}/liftover/${TF}.consensus_summits_to_${target}.bed &"
 
     # Lift peaks
     echo "halLiftover --bedType 4 ${pathHAL} ${sp} ${pathData}/${TF}.consensus_peaks.bed ${target} \
-          ${pathResults}/${TF}.consensus_peaks_to_${target}.bed"
+          ${pathResults}/liftover/${TF}.consensus_peaks_to_${target}.bed"
     echo "wait"
 
     # Merge lifted regions with HALPER
-    echo "python3 -m orthologFind -max_frac 1.2 -min_frac 0.8 -protect_dist 5 -qFile ${pathData}/${TF}.consensus_peaks.bed \
-          -tFile ${pathResults}/${TF}.consensus_peaks_to_${target}.bed -sFile ${pathResults}/${TF}.consensus_summits_to_${target}.bed \
+    echo "python3 -m orthologFind -max_len 1000 -min_len 50 -protect_dist 5 \
+          -qFile ${pathData}/${TF}.consensus_peaks.bed \
+          -tFile ${pathResults}/liftover/${TF}.consensus_peaks_to_${target}.bed \
+          -sFile ${pathResults}/liftover/${TF}.consensus_summits_to_${target}.bed \
           -oFile ${pathResults}/HALPER_${TF}_${sp}2${target}.bed -mult_keepone"
 
   } >> "${logFile}"
