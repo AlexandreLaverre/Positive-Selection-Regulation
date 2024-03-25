c668cf06adf0831f750c4702da0bfde5ed6954e4
Alexandre <alexandre.laverre@unil.ch>
2024-03-25 19:39:14 +0100

    convert peaks and summits at the same time

--- utils/convert.BED.chrNames.py
+++ utils/convert.BED.chrNames.py
@@ -5,16 +5,13 @@ import sys
 sp = sys.argv[1]
 sample = sys.argv[2]
 TF = sys.argv[3]
-data = sys.argv[4]
 
-path = "/work/FAC/FBM/DEE/mrobinso/evolseq/DetectPosSel" if sys.argv[5] == "cluster" \
+path = "/work/FAC/FBM/DEE/mrobinso/evolseq/DetectPosSel" if sys.argv[4] == "cluster" \
     else "/Users/alaverre/Documents/Detecting_positive_selection/"
 
-BED = f"{path}/results/peaks_calling/NarrowPeaks/{sp}/{sample}/bowtie2/mergedLibrary/macs2/narrowPeak/consensus/{TF}/{TF}.consensus_{data}.bed"
-Correspondence = f"{path}/data/genome_sequences/{sp}/chromosome_correspondence.txt"
-output = f"{path}/results/peaks_calling/NarrowPeaks/{sp}/{sample}/{TF}.consensus_{data}_UCSC.bed"
+Correspondence = f"{path}/data/genome_sequences/{sp}/chromosome_correspondence_Ensembl2UCSC.txt"
+Consensus = f"{path}/results/peaks_calling/NarrowPeaks/{sp}/{sample}/bowtie2/mergedLibrary/macs2/narrowPeak/consensus/"
 
-outfile = open(output, 'w')
 ####################################################################################################
 Correspondence_dict = {}
 with open(Correspondence, 'r') as f1:
@@ -23,17 +20,20 @@ with open(Correspondence, 'r') as f1:
         i = i.split("\t")
         Correspondence_dict[i[0]] = i[1]
 
-with open(BED, 'r') as f2:
-    for i in f2.readlines():
-        i = i.strip("\n")
-        i = i.split("\t")
+for data in ["peaks", "consensus_summits"]:
+    BED = f"{Consensus}/{TF}/{TF}.{data}.bed"
+    outfile = open(f"{path}/results/peaks_calling/NarrowPeaks/{sp}/{sample}/{TF}.{data}_UCSC_names.bed", 'w')
+    with open(BED, 'r') as f2:
+        for i in f2.readlines():
+            i = i.strip("\n")
+            i = i.split("\t")
 
-        old = str(i[0])
-        if old in Correspondence_dict.keys():
-            new_chr = str(Correspondence_dict[old])
-            new_ID = f"{new_chr}:{str(i[1])}:{str(i[2])}"
-            
-            outfile.write("\t".join([new_chr, str(i[1]), str(i[2]), str(i[3]), new_ID]) + '\n')
+            old = str(i[0])
+            if old in Correspondence_dict.keys():
+                new_chr = str(Correspondence_dict[old])
+                new_ID = f"{new_chr}:{str(i[1])}:{str(i[2])}"
 
-outfile.close()
+                outfile.write("\t".join([new_chr, str(i[1]), str(i[2]), str(i[3]), new_ID]) + '\n')
+    
+    outfile.close()
 ####################################################################################################
