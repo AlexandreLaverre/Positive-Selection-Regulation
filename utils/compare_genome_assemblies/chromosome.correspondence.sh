e5c6edfdeea2bea62abfc4655cabadd1b30148a9
Alexandre <alexandre.laverre@unil.ch>
2024-03-25 18:08:03 +0100

    simplify path

--- utils/compare_genome_assemblies/chromosome.correspondence.sh
+++ utils/compare_genome_assemblies/chromosome.correspondence.sh
@@ -13,11 +13,9 @@ else
 	export path=/Users/alaverre/Documents/Detecting_positive_selection/
 fi
 
-export pathScripts=${path}/scripts/compare_genome_assemblies
-
 ####################################################################################
 
-perl ${pathScripts}/chromosome.correspondence.pl --pathAssembly1=${genome1} \
+perl ${path}/scripts/utils/compare_genome_assemblies/chromosome.correspondence.pl --pathAssembly1=${genome1} \
       --pathAssembly2=${genome2} --pathOutput=${pathGenomes}/chromosome_correspondence_${suffix}.txt
 
 ####################################################################################
