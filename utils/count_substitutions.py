f2cbe33c889d58fef8b503085f4fa590467016eb
Alexandre <alexandre.laverre@unil.ch>
2024-04-04 17:48:00 +0200

    add GC_content and all possible substitutions

--- utils/count_substitutions.py
+++ utils/count_substitutions.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python
 # coding=utf-8
-import sys
-from pathlib import Path
 from Bio import SeqIO
+import itertools
+import argparse
 
-path = "/Users/alaverre/Documents/Detecting_positive_selection/results/"
+####################################################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument("sp", help="Species name: human dog")
+parser.add_argument("sample", help="Study name: Wilson Schmidt...")
+parser.add_argument("TF", help="Transcription Factor: CEBPA CTCF ...")
+parser.add_argument("peakType", help="NarrowPeaks or BroadPeaks")
+args = parser.parse_args()
 
-Ref_Path = path + sys.argv[1]
-Target_Path = path + sys.argv[2]
-Output = open(str(Path(Ref_Path).parent) + "/substitutions.txt", "w")
+path = f"/work/FAC/FBM/DEE/mrobinso/evolseq/DetectPosSel/results/positive_selection/" \
+       f"{args.peakType}/{args.sp}/{args.sample}/{args.TF}/"
 
+Ref_Path = f"{path}/sequences/filtered_ancestral_sequences.fa"
+Target_Path = f"{path}/sequences/filtered_focal_sequences_upper.fa"
+Output = open(f"{path}/sequences/focal_substitutions_stats.txt", "w")
 
+nuc = ['A', 'T', 'C', 'G']
+combinations = itertools.permutations(nuc, 2)
+
+########################################################################################################################
 # Get substitutions of each type per sequence
 def get_sub_number(seq_ref, seq_alt):
     if len(seq_ref) != len(seq_alt):
         raise ValueError("Reference and target sequences don't have the same length!")
 
-    substitutions = {"Weak2Weak": 0, "Weak2Strong": 0, "Strong2Weak": 0, "Strong2Strong": 0}
+    subs = {"".join(sub): 0 for sub in combinations}
+
     for base1, base2 in zip(seq_ref, seq_alt):
         if base1 != base2:
-            base1_type = True if base1 in "AT" else False  # Weak == True, Strong == False
-            base2_type = True if base2 in "AT" else False
+            subs[f"{base1}{base2}"] += 1
 
-            if base1_type:
-                sub_type = "Weak2Weak" if base2_type else "Weak2Strong"
-            else:
-                sub_type = "Strong2Weak" if base2_type else "Strong2Strong"
+    subs["Weak2Weak"] = subs["AT"] + subs["TA"]
+    subs["Weak2Strong"] = subs["AC"] + subs["AG"] + subs["TC"] + subs["TG"]
+    subs["Strong2Strong"] = subs["GC"] + subs["CG"]
+    subs["Strong2Weak"] = subs["CA"] + subs["CT"] + subs["GA"] + subs["GT"]
 
-            substitutions[sub_type] += 1
+    return subs
 
-    return substitutions
-
-
+########################################################################################################################
 # Get Reference sequences
 ReferenceSeqs = SeqIO.to_dict(SeqIO.parse(open(Ref_Path), "fasta"))
 if len(ReferenceSeqs) == 0:
@@ -43,17 +53,24 @@ SeqIDs = TargetSeqs.keys()
 if len(TargetSeqs) == 0:
     raise ValueError("Focal sequence file is empty!")
 
+########################################################################################################################
 
-Output.write('\t'.join(["ID", "Length", "NbSub", "Weak2Weak", "Weak2Strong", "Strong2Weak", "Strong2Strong"]) + "\n")
+permutation = ["".join(sub) for sub in combinations]
+Output.write('\t'.join(["ID", "Length", "GC_Content", "NbSub"]) + '\t' + '\t'.join(permutation) + '\t' +
+             '\t'.join(["Weak2Weak", "Weak2Strong", "Strong2Strong", "Strong2Weak"]) + "\n")
+
 for ID in SeqIDs:
     ref_seq = str(ReferenceSeqs[ID].seq)
     target_seq = str(TargetSeqs[ID].seq)
+    GC_content = str(float((target_seq.count('G') + target_seq.count('C'))) / len(target_seq) * 100)
 
     substitutions = get_sub_number(ref_seq, target_seq)
     substitution_number = list(substitutions.values())
     Nsub = str(sum(substitution_number))
 
-    Output.write('\t'.join([ID, str(len(ref_seq)), Nsub, '\t'.join(map(str, substitution_number))]) + "\n")
+    Output.write('\t'.join([ID, str(len(ref_seq)), GC_content, Nsub, '\t'.join(map(str, substitution_number))]) + "\n")
 
 
 Output.close()
+
+########################################################################################################################
