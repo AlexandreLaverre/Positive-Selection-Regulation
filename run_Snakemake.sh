abe3f5f6657adc30e9b298d96169bbf04d5e9104
Alexandre <alexandre.laverre@unil.ch>
2024-05-15 10:18:36 +0200

    tmp trigger mtime

--- run_Snakemake.sh
+++ run_Snakemake.sh
@@ -26,7 +26,7 @@ conda activate TestPos
 
 ##################################################################
 # mtime or input or params
-snakemake ${dryRun} --rerun-triggers input -j 64 --config sp=${species} sample=${sample}  \
+snakemake ${dryRun} --rerun-triggers mtime -j 64 --config sp=${species} sample=${sample}  \
           cluster=${cluster} nbPart=${nbThreads} --rerun-incomplete \
           --cluster "sbatch -p cpu -N 1 -o ${pathLog}/slurm.out_${Prefix} -e ${pathLog}/slurm.err_${Prefix} \
           -c {params.threads} --mem={params.mem} -t {params.time}"
