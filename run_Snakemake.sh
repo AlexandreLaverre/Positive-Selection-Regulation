67bc8c652417554b33105498774fc80eb3fa7483
Alexandre <alexandre.laverre@unil.ch>
2024-05-15 10:13:35 +0200

    tmp clean metadata

--- run_Snakemake.sh
+++ run_Snakemake.sh
@@ -29,7 +29,9 @@ conda activate TestPos
 snakemake ${dryRun} --rerun-triggers input -j 64 --config sp=${species} sample=${sample}  \
           cluster=${cluster} nbPart=${nbThreads} --rerun-incomplete \
           --cluster "sbatch -p cpu -N 1 -o ${pathLog}/slurm.out_${Prefix} -e ${pathLog}/slurm.err_${Prefix} \
-          -c {params.threads} --mem={params.mem} -t {params.time}"
+          -c {params.threads} --mem={params.mem} -t {params.time}" --cleanup-metadata ['../results/peaks_calling/NarrowPeaks/human/Wilson/CEBPA.consensus_summits.bed', '../results/peaks_calling/NarrowPeaks/human/Wilson/CEBPA.peaks_UCSC_names.bed']
 
-#--cleanup-metadata ../results/peaks_calling/NarrowPeaks/human/Wilson/CEBPA.consensus_summits.bed
+
+#--cleanup-metadata ['../results/peaks_calling/NarrowPeaks/human/Wilson/CEBPA.consensus_summits.bed', '../results/peaks_calling/NarrowPeaks/human/Wilson/CEBPA.peaks_UCSC_names.bed']
+
 ##################################################################
