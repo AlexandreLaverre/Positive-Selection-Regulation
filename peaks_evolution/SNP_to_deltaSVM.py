9e04768cf933020c65879b1128f64c35a6b389ea
Alexandre <alexandre.laverre@unil.ch>
2024-05-21 14:45:08 +0200

    add min_max SVM and flag

--- peaks_evolution/SNP_to_deltaSVM.py
+++ peaks_evolution/SNP_to_deltaSVM.py
@@ -6,14 +6,6 @@ from Bio import SeqIO
 import pandas as pd
 import gzip
 
-#path = '/Users/alaverre/Documents/Detecting_positive_selection/cluster/results/'
-#vcf_file = path + 'polymorphism_analyses/NarrowPeaks/human/Wilson/CEBPA/VCF/filtered_chr21.vcf.gz'
-#deltaSVM_file = path + 'positive_selection/NarrowPeaks/human/Wilson/CEBPA/deltas/ancestral_all_possible_deltaSVM.txt'
-#seq_file = path + 'positive_selection/NarrowPeaks/human/Wilson/CEBPA/sequences/filtered_focal_sequences_upper.fa'
-#maxLL_file = path + 'positive_selection/NarrowPeaks/human/Wilson/CEBPA/MLE_summary_50bins.csv'
-#output_file = path + 'polymorphism_analyses/NarrowPeaks/human/Wilson/CEBPA/SNP_to_deltaSVM.txt'
-#genome_file = "/Users/alaverre/Documents/Detecting_positive_selection/data/genome_sequences/human/hg38.fa.gz"
-
 VCF = pd.read_csv(sys.argv[1], sep='\t', header=None, compression='gzip', skiprows=20)
 DeltaSVM = pd.read_csv(sys.argv[2], sep='\t', header=0)
 FocalSeq = SeqIO.to_dict(SeqIO.parse(open(sys.argv[3]), "fasta"))
@@ -32,7 +24,7 @@ VCF.columns = header + ["chr", "start", "end", "PeakID"]
 # Write header
 tab = '\t'
 pop = ['EAS_AF', 'EUR_AF', 'AFR_AF', 'AMR_AF']
-output.write("ID\tPos\tRef\tAlt\t"+'\t'.join(pop)+"\tDeltaSVM\tStabParam\tAlphaPos\tBetaPos\tLength\n")
+output.write("ID\tPos\tRef\tAlt\t"+'\t'.join(pop)+"\tDeltaSVM\tStabParam\tAlphaPos\tBetaPos\tLength\tMin\tMax\tFlag\n")
 
 for SNP in VCF.iterrows():
     ID = SNP[1]['PeakID']
@@ -64,21 +56,27 @@ for SNP in VCF.iterrows():
 
     # Relative position in sequence and frequency in populations
     pos = SNP_pos - start
-    freq = [pop.split('=')[1] for pop in SNP[1]['INFO'].split(';')[4:8]]
+    assert genome[chr].seq[SNP_pos].upper() == FocalSeq[ID].seq[pos], "Position does not correspond to the genome sequence."
 
     # Check that ref or alt allele is in ancestral sequence and get deltaSVM
     allSVM = DeltaSVM.loc[DeltaSVM['ID'] == ID, "pos0:A":].iloc[0]
+    svm_bounds = [min(allSVM), max(allSVM)]
+    freq = [pop.split('=')[1] for pop in SNP[1]['INFO'].split(';')[4:8]]
 
     # Ref = Ancestral
     if pd.isna(allSVM[f"pos{pos}:{ref}"]):
         SNP_deltaSVM = allSVM[f"pos{pos}:{alt}"]
+        flag = "ref_ancestral"
     # Alt = Ancestral
     elif pd.isna(allSVM[f"pos{pos}:{alt}"]):
         SNP_deltaSVM = -allSVM[f"pos{pos}:{ref}"]
-        freq = [str(1-float(pop)) for pop in freq]
+        flag = "alt_ancestral"
     else:
         continue
 
     # Write output
     output.write(f"{ID}\t{pos}\t{ref}\t{alt}\t{tab.join(freq)}\t{SNP_deltaSVM}\t{stab_param}"
-                 f"\t{pos_params['AlphaPos']}\t{pos_params['BetaPos']}\t{original_length}\n")
+                 f"\t{pos_params['AlphaPos']}\t{pos_params['BetaPos']}\t{original_length}"
+                 f"\t{tab.join(svm_bounds)}\t{flag}\n")
+
+output.close()
