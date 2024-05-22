c1f80b24008cd2dde76537cc77344b563cf1d264
Alexandre <alexandre.laverre@unil.ch>
2024-05-22 18:14:18 +0200

    add conclusion

--- peaks_evolution/SNP_to_deltaSVM.py
+++ peaks_evolution/SNP_to_deltaSVM.py
@@ -27,7 +27,8 @@ VCF.columns = header + ["chr", "start", "end", "PeakID"]
 # Write header
 tab = '\t'
 pop = ['EAS_AF', 'EUR_AF', 'AFR_AF', 'AMR_AF']
-output.write("ID\tPos\tRef\tAlt\tNbAlt\tNbTot\t"+'\t'.join(pop)+"\tLength\tFlag\tDeltaSVM\tStabParam\tSelCoefStab\tAlphaPos\tBetaPos\tSelCoefPos\n")
+output.write("ID\tPos\tRef\tAlt\tNbAlt\tNbTot\t"+'\t'.join(pop) + "\tLength\tFlag\t" +
+             "DeltaSVM\tConclusion\tStabParam\tSelCoefStab\tAlphaPos\tBetaPos\tSelCoefPos\n")
 
 for SNP in VCF.iterrows():
     ID = SNP[1]['PeakID']
@@ -58,6 +59,7 @@ for SNP in VCF.iterrows():
     else:
         Stab_param = MaxLL.loc[MaxLL['ID'] == ID, "AlphaPurif"].iloc[0]
         Pos_params = MaxLL.loc[MaxLL['ID'] == ID, ["AlphaPos", "BetaPos"]].iloc[0]
+        Conclusion = MaxLL.loc[MaxLL['ID'] == ID, "Conclusion"].iloc[0]
 
     # Relative position in sequence
     pos = SNP_pos - start
@@ -84,7 +86,7 @@ for SNP in VCF.iterrows():
 
     # Write output
     output.write(f"{ID}\t{pos}\t{ref}\t{alt}\t{nb_alt}\t{nb_tot}\t{tab.join(freq)}\t{original_length}\t{flag}\t"
-                 f"{SNP_deltaSVM}\t{Stab_param}\t{Coef_Stab}\t"
+                 f"{SNP_deltaSVM}\t{Conclusion}\t{Stab_param}\t{Coef_Stab}\t"
                  f"{Pos_params['AlphaPos']}\t{Pos_params['BetaPos']}\t{Coef_Pos}\n")
 
 output.close()
