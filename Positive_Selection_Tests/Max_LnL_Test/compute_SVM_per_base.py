25c985ac65c7ee06ca6f82eff304393c6a045ad2
Alexandre <alexandre.laverre@unil.ch>
2024-06-19 16:30:09 +0200

    init SVM per base

---
+++ Positive_Selection_Tests/Max_LnL_Test/compute_SVM_per_base.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import argparse
+from Bio import SeqIO
+from alive_progress import alive_bar
+import multiprocessing.pool
+import sys
+
+########################################################################################################################
+# Variables and paths
+parser = argparse.ArgumentParser()
+parser.add_argument("species", help="Species name: human dog ...")
+parser.add_argument("sample", help="Study name and Transcription Factor: Wilson/CEBPA Schmidt/CTCF ...")
+parser.add_argument("--peakType", default="NarrowPeaks", help="NarrowPeaks or BroadPeaks")
+parser.add_argument("-T", "--NbThread", default=1, type=int, help="Number of threads for parallelization (default=8)")
+args = parser.parse_args()
+maxLen = 1000
+
+path = "/Users/alaverre/Documents/Detecting_positive_selection/"
+pathResults = f"{path}/cluster/results/positive_selection/{args.peakType}/{args.species}/{args.sample}"
+sys.path.append(f"{path}/scripts/Positive_Selection_Tests/functions/")
+import SVM
+
+
+def run_svm_pos(seq_name):
+    focal_seq = str(FocalSeqs[seq_name].seq)
+    svm_pos = []
+    for pos in range(len(focal_seq)):
+        svm_pos.append(str(SVM.calculate_svm_per_pos(focal_seq, pos, SVM_dict)))
+
+    return seq_name, svm_pos
+
+
+########################################################################################################################
+# Binding affinity values per kmer
+SVM_dict = SVM.get_svm_dict(f"{pathResults}/Model/kmer_predicted_weight.txt")
+
+# Get sequences
+FocalSeqs = SeqIO.to_dict(SeqIO.parse(open(f"{pathResults}/sequences/filtered_focal_sequences.fa"), "fasta"))
+SeqIDs = FocalSeqs.keys()
+
+output = open(f"{pathResults}/SVM_per_base.txt", "w")
+all_pos = '\t'.join([f"pos{i}" for i in range(maxLen)])
+output.write(f"ID\t{all_pos}\n")
+
+# Running and writing results
+if __name__ == '__main__':
+    with alive_bar(len(SeqIDs)) as bar:  # progress bar
+        with multiprocessing.Pool(args.NbThread) as pool:
+            for svm in pool.imap_unordered(run_svm_pos, SeqIDs):
+                bar()
+                output.write(str(svm[0]) + '\t' + '\t'.join(svm[1]) + '\n')
+
+output.close()
+########################################################################################################################
