7ad14d653892a3c6ee289957c3df6ac8e3baf5a1
Alexandre <alexandre.laverre@unil.ch>
2024-06-20 14:44:19 +0200

    compute using readsCount

--- ChIPseq_analyses/peaks_base_coverage.sh
+++ ChIPseq_analyses/peaks_base_coverage.sh
@@ -30,22 +30,23 @@ for pathBam in "$pathPeaks"/*"$TF"*bam; do
     # Obtain normalized reads count
     echo "bamCompare -b1 ${pathBam} \
     -b2 ${pathPeaks}/${input} \
-    -o ${pathResults}/${TF}/${indiv}_bgNorm_SES.bw \
+    -o ${pathResults}/${TF}/${indiv}_bgNorm_readCount.bw \
     --binSize 1 \
-    --scaleFactorsMethod SES \
-    --sampleLength 1000 \
-    --centerReads \
+    --scaleFactorsMethod readCount \
+    --effectiveGenomeSize 2913022398 \
+    --region 20 \
     -p ${threads} 2> ${pathPeaks}/deepTools/coverage/logs/${TF}_${indiv}_bamCompare.log"
+    #    --centerReads --sampleLength 1000 --normalizeUsing RPKM \
 
     # Matrix for each sample
     echo "computeMatrix reference-point --referencePoint TSS \
     -a 1000 -b 0 \
     -bs 1 \
     -R ${pathPeaks}/macs2/narrowPeak/consensus/${TF}/${TF}.consensus_peaks.bed \
-    -S ${pathResults}/${TF}/${indiv}_bgNorm_SES.bw \
-    -o ${pathResults}/${TF}/${indiv}_matrix_SES.gz \
+    -S ${pathResults}/${TF}/${indiv}_bgNorm_readCount.bw \
+    -o ${pathResults}/${TF}/${indiv}_matrix_readCount.gz \
     -p ${threads} \
-    --outFileSortedRegions ${pathResults}/${TF}/${indiv}_peaks_SES.bed"
+    --outFileSortedRegions ${pathResults}/${TF}/${indiv}_peaks_readCount.bed"
 
   } > "${logFile}"
 
