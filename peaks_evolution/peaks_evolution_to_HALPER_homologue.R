be17a29d67e145ecc32e0383999933609776b43e
Alexandre <alexandre.laverre@unil.ch>
2024-07-10 11:33:22 +0200

    correct error in IDs, add plots

--- peaks_evolution/peaks_evolution_to_HALPER_homologue.R
+++ peaks_evolution/peaks_evolution_to_HALPER_homologue.R
@@ -1,8 +1,11 @@
 library(ape)
+library(corrplot)
+library(RColorBrewer)
+
 
 # Define paths
 path <- "/Users/alaverre/Documents/Detecting_positive_selection/results/"
-sp="mouse"
+sp="human"
 sample=c("Wilson", "Schmidt12")
 TFs=c("CTCF", "CEBPA", "FOXA1", "HNF4A", "HNF6")
 
@@ -11,29 +14,86 @@ TFs=c("CTCF", "CEBPA", "FOXA1", "HNF4A", "HNF6")
 all_MLE <- list()
 for (samp in sample){
   for (TF in TFs){
-    MLE.file <- paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "_", samp, "_", TF, "_MLE_summary_50bins.csv")
+    MLE.file <- paste0(path, "positive_selection/NarrowPeaks/", sp, "/", samp, "_", TF, "_MLE_summary_50bins.csv")
     
     if (file.exists(MLE.file)){
       MLE <- read.csv(MLE.file, h=T, row.names = 1)
+      
+      #correct error in IDs
+      rownames(MLE) <- gsub("\\d+:\\d+:\\d+:", "", rownames(MLE))
+      rownames(MLE) <- gsub("_([[:alpha:]])+:\\d+:\\d+:", "_", rownames(MLE))
+      MLE$peaks_ID <- gsub(".*:\\d+:\\d+_", "", rownames(MLE))
+      
       MLE$Conclusion <- as.factor(MLE$Conclusion)
       MLE$Conclusion <- factor(MLE$Conclusion, levels=c("Positive model", "Stabilizing model", "Neutral model"))
       MLE$ID <- rownames(MLE)
-      MLE$species <- sp
       
       all_MLE[[TF]] <- MLE
     }
     
-    positive <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Positive model"),]
-    neutral <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Neutral model"),]
-    stabilising <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Stabilizing model"),]
+    #positive <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Positive model"),]
+    #neutral <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Neutral model"),]
+    #stabilising <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Stabilizing model"),]
     
-    write(gsub(":", "\t", all_MLE[[TF]]$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_all.bed"))
-    write(gsub(":", "\t", positive$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_positive.bed"))
-    write(gsub(":", "\t", neutral$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_neutral.bed"))
-    write(gsub(":", "\t", stabilising$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_stabilising.bed"))
+    #write(gsub(":", "\t", all_MLE[[TF]]$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_all.bed"))
+    #write(gsub(":", "\t", positive$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_positive.bed"))
+    #write(gsub(":", "\t", neutral$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_neutral.bed"))
+    #write(gsub(":", "\t", stabilising$ID), file=paste0(path, "MaxLikelihoodApproach/real_peaks/", sp, "/", TF, "_stabilising.bed"))
     }
 }
 
+################################################################################
+suffix=if (sp == "human") "17way" else "60way.glire"
+
+all_phyloP <- list()
+for (TF in TFs){
+  phyloP <- read.table(paste0(path, "phyloP/", sp, "/", TF, "_", suffix, ".txt"), h=T)
+  rownames(phyloP) <- phyloP$ID
+  phyloP <- phyloP[rownames(all_MLE[[TF]]),]
+  phyloP$class <- cut(phyloP$Score, quantile(phyloP$Score, probs = seq(0, 1, 0.2),
+                                             na.rm=T), include.lowest=T, 
+                      labels=c("20%", "40%", "60%", "80%", "100%"))
+
+  all_phyloP[[TF]] <- phyloP
+}
+
+################################################################################
+par(xpd=T)
+pdf(paste0(path, "figures/PhyloP_MaxLLTest_human.pdf"))
+for (TF in TFs){
+  par(mfrow=c(2,2))
+  phyloP <- all_phyloP[[TF]]
+  MLE <- all_MLE[[TF]]
+  MLE$phyloP <- phyloP$Score
+  boxplot(phyloP$Score~MLE$Conclusion, notch=T, outline=F, xlab="Conclusion", ylab="PhyloP score")
+  
+
+  prop <- prop.table(table(MLE$Conclusion, phyloP$class), margin = 2)
+  barplot(prop, col=c("forestgreen", "deepskyblue2", "firebrick"), las=1, xlab="class phyloP", ylab="Proportion", main=TF)
+  legend("top", fill=c("forestgreen", "deepskyblue2", "firebrick"), legend=c("Positive", "Negative", "Neutral"),
+         bty="n", ncol=3, y.intersp = -3)
+  
+  MLE$Conclusion <- NULL
+  MLE$ID <- NULL
+  CorMat <- cor(MLE, use="pairwise.complete.obs")
+  corrplot(CorMat, type="upper", order="hclust", col=brewer.pal(n=8, name="RdYlBu"))
+}
+dev.off()
+
+par(mfrow=c(2,2))
+#deltaSVM
+plot(PosSel$deltaSVM~phyloP$Score, cex=0.1, main=paste(sp, TF), xlab="phyloP score", ylab="deltaSVM")
+cor = cor.test(phyloP$Score, PosSel$deltaSVM)
+mtext(paste0("R2=", round(cor$estimate, 2)), cex=0.8)
+
+boxplot(PosSel$deltaSVM~phyloP$class, notch=T, xlab="phyloP score quantile", ylab="deltaSVM", las=1, outline=F)
+
+#deltaSVM pval
+plot(PosSel$pval.high~phyloP$Score, cex=0.1, main="", xlab="phyloP score", ylab="deltaSVM pvalue")
+cor = cor.test(phyloP$Score, PosSel$pval.high)
+
+mtext(paste0("R2=", round(cor$estimate, 2)), cex=0.8)
+boxplot(PosSel$pval.high~phyloP$class, notch=T, xlab="phyloP score quantile", ylab="deltaSVM pvalue", las=1)
 
 ################################################################################
 # Retrieve HALPER homologous regions
@@ -78,23 +138,18 @@ for (TF in TFs){
 # Plot proportion homologue ~ peaks evolution
 par(mfrow=c(2,2))
 for (TF in TFs){
-  ###################### Attribute peaks ID: TEMPORARY!!! ######################
-  all_MLE[[TF]]$peak_ID <- sample(rownames(all_HALPER[[TF]]), nrow(all_MLE[[TF]]), replace=F)
-  rownames(all_MLE[[TF]]) <- all_MLE[[TF]]$peak_ID
-  ##############################################################################
-  
   positive <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Positive model"),]
   neutral <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Neutral model"),]
   stabilising <- all_MLE[[TF]][which(all_MLE[[TF]]$Conclusion == "Stabilizing model"),]
   
-  prop_homolog_positive <- apply(all_HALPER[[TF]][rownames(positive),], 2, function(y) sum(!is.na(y))/length(y))
-  prop_homolog_neutral <- apply(all_HALPER[[TF]][rownames(neutral),], 2, function(y) sum(!is.na(y))/length(y))
-  prop_homolog_stabilising <- apply(all_HALPER[[TF]][rownames(stabilising),], 2, function(y) sum(!is.na(y))/length(y))
+  prop_homolog_positive <- apply(all_HALPER[[TF]][positive$peaks_ID,], 2, function(y) sum(!is.na(y))/length(y))
+  prop_homolog_neutral <- apply(all_HALPER[[TF]][neutral$peaks_ID,], 2, function(y) sum(!is.na(y))/length(y))
+  prop_homolog_stabilising <- apply(all_HALPER[[TF]][stabilising$peaks_ID,], 2, function(y) sum(!is.na(y))/length(y))
   
-  plot(prop_homolog_neutral~DivTree, ylab="Proportion homologous peak", xlab="Divergence Time", type="b", main=TF, ylim=c(0.5, 1))
+  plot(prop_homolog_neutral~DivTree, ylab="Proportion homologous peak", xlab="Divergence Time", type="b", main=TF, ylim=c(0.4, 1))
   lines(prop_homolog_positive~DivTree, type="b", col="forestgreen")
   lines(prop_homolog_stabilising~DivTree, type="b", col="red")
-  legend("topright", legend=c("Negative", "Neutral", "Positive"), col=c("red", "black", "forestgreen"), lty=1, bty="n")
+  legend("topright", legend=c("Stabilising", "Neutral", "Positive"), col=c("red", "black", "forestgreen"), lty=1, bty="n")
 }
 ################################################################################
 
