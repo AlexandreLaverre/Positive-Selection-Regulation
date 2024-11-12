d4cd2e360392ad13fd4e6a05bed30d75be28c5ee
Alexandre <alexandre.laverre@unil.ch>
2024-11-12 16:07:35 +0100

    add simul option

--- Positive_Selection_Tests/Max_LnL_Test/run_MaxLL_estimation.sh
+++ Positive_Selection_Tests/Max_LnL_Test/run_MaxLL_estimation.sh
@@ -5,6 +5,7 @@ export sample=$2				# i.e: Wilson/CEBPA Schmidt/HNF4A ...
 export nbThread=$3				# i.e: int (number of part for parallelization)
 export nbBin=$4					# i.e: int (number of bins for all deltas distribution)
 export cluster=$5				# i.e: local or cluster
+export simul=${6:-False}				# i.e: int (number of bins for all deltas distribution)
 
 export Prefix=${species}_${sample/\//_}
 
@@ -15,7 +16,7 @@ if [ ${cluster} = "cluster" ]; then
 	export pathScripts=/work/FAC/FBM/DEE/mrobinso/evolseq/DetectPosSel/scripts/Positive_Selection_Tests/Max_LnL_Test/
 
 	# Calculate time needed: considering 1500 peaks per hour and per thread
-	export time=$(( ($(wc -l < ${path}/results/positive_selection/${species}/${sample}/deltas/ancestral_to_observed_deltaSVM.txt) / (1500*${nbThread}))+1 ))
+	export time=$(( ($(wc -l < ${path}/results/positive_selection/NarrowPeaks/${species}/${sample}/deltas/ancestral_to_observed_deltaSVM.txt) / (1500*${nbThread}))+1 ))
 
 	# BATCH informations
 	echo "#!/bin/sh" > ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
@@ -30,7 +31,7 @@ if [ ${cluster} = "cluster" ]; then
 	echo "source /work/FAC/FBM/DEE/mrobinso/evolseq/Tools/mambaforge/etc/profile.d/conda.sh" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 	echo "conda activate TestPos" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 
-	echo "python ${pathScripts}/MaxLL_estimation.py ${species} ${sample} -T ${nbThread} --NbBin ${nbBin} --cluster" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
+	echo "python ${pathScripts}/MaxLL_estimation.py ${species} ${sample} -T ${nbThread} --NbBin ${nbBin} -S ${simul} --cluster" >> ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 	sbatch ${pathScripts}/logs/bsub_script_MaxLL_${Prefix}
 
 else
