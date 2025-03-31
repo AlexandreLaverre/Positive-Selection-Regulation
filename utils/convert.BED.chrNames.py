c1039cb0f3eee53b652185319e68790ecba199fe
Alexandre <alexandre.laverre@unil.ch>
2025-04-01 00:35:19 +0200

    adjust UCSC

--- utils/convert.BED.chrNames.py
+++ utils/convert.BED.chrNames.py
@@ -17,17 +17,6 @@ pathMatrix = f"{path}/results/substitution_matrix/{sp}/"
 chromosomes = [os.path.splitext(file)[0] for file in os.listdir(pathMatrix)]
 
 ####################################################################################################
-
-# If suffix == '_UCSC_names', copy original file to new file.
-if suffix == '_UCSC_names':
-    for data in ["peaks", "consensus_summits"]:
-        BED = f"{pathPeaks}/{TF}.{data}.bed"
-        if not os.path.exists(BED):
-            continue
-        os.system(f"cut -f1-4 {BED} > {pathPeaks}/{TF}.{data}_UCSC_names.bed")
-    sys.exit()
-
-
 Correspondence_dict = {}
 with open(Correspondence, 'r') as f1:
     for i in f1.readlines():
@@ -44,16 +33,23 @@ for data in ["peaks", "consensus_summits"]:
         for i in f2.readlines():
             i = i.strip("\n")
             i = i.split("\t")
-
             old = str(i[0])
-            peak_ID = str(i[3]) if data == "consensus_summits" else str(i[3].split(":")[3])
+
+            if data == "consensus_summits" or suffix == '_UCSC_names':
+                peak_ID = str(i[3])
+            else:
+                peak_ID = str(i[3].split(":")[3])
+
             if old in Correspondence_dict.keys():
                 new_chr = str(Correspondence_dict[old])
 
-                # Filter chromosomes based on substitution matrix.
-                if old in chromosomes or new_chr in chromosomes:
-                    new_ID = f"{new_chr}:{str(i[1])}:{str(i[2])}_{peak_ID}"
-                    outfile.write("\t".join([new_chr, str(i[1]), str(i[2]), new_ID]) + '\n')
+            if suffix == '_UCSC_names':
+                new_chr = old
+
+            # Filter chromosomes based on substitution matrix.
+            if old in chromosomes or new_chr in chromosomes:
+                new_ID = f"{new_chr}:{str(i[1])}:{str(i[2])}_{peak_ID}"
+                outfile.write("\t".join([new_chr, str(i[1]), str(i[2]), new_ID]) + '\n')
 
     outfile.close()
 ####################################################################################################
