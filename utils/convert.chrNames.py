9833276202d73740e9721be926f96bef446aece7
Alexandre <alexandre.laverre@unil.ch>
2025-07-29 13:22:52 +0200

    first commit

---
+++ utils/convert.chrNames.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+import sys
+
+pathCorrespondence = sys.argv[1]
+pathBED = sys.argv[2]
+output = open(sys.argv[3])
+
+Correspondence_dict = {}
+with open(pathCorrespondence, 'r') as f1:
+    for i in f1.readlines():
+        i = i.strip("\n")
+        i = i.split("\t")
+        Correspondence_dict[i[0]] = i[1]
+
+with open(pathBED, 'r') as f1:
+    for i in f1.readlines():
+        i = i.strip("\n").split("\t")
+        old = str(i[0])
+
+        # skip header
+        if old == "chrom":
+            continue
+
+        if old in Correspondence_dict.keys():
+            new_chr = str(Correspondence_dict[old])
+
+        output.write("\t".join([new_chr, str(i[1]), str(i[2]), str(i[3])]) + '\n')
+
+output.close()
+####################################################################################################
