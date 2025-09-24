afce78b612034f7ee25aae06a59367c874ea5f7a
Alexandre <alexandre.laverre@unil.ch>
2025-09-24 13:24:56 +0200

    remove mention of cluster

--- 2_selection_tests/run_Snakemake.sh
+++ 2_selection_tests/run_Snakemake.sh
@@ -2,9 +2,8 @@
 
 export species=$1				  # i.e: human dog ...
 export sample=$2				  # i.e: Wilson Schmidt12 ...
-export cluster=$3				  # i.e: local or cluster
-export nbThreads=$4				# i.e: int (number of part for parallelization)
-export dryRun=${5:-""}		# i.e: -n or nothing (run snakemake in dry-run mode)"
+export nbThreads=$3				# i.e: int (number of part for parallelization)
+export dryRun=${4:-""}		# i.e: -n or nothing (run snakemake in dry-run mode)"
 
 export Prefix=${species}_${sample}
 
@@ -22,8 +21,7 @@ if [ "${dryRun}" = "-n" ]; then
 	echo "Run Snakemake in dry-run mode"
 fi
 
-snakemake ${dryRun} --rerun-triggers mtime -j 64 --config sp=${species} sample=${sample}  \
-          cluster=${cluster} nbPart=${nbThreads} --rerun-incomplete \
+snakemake ${dryRun} --rerun-triggers mtime -j 64 --config sp=${species} sample=${sample} nbPart=${nbThreads} --rerun-incomplete \
           --cluster "sbatch -p cpu -N 1 -o ${pathLog}/slurm.out_${Prefix} -e ${pathLog}/slurm.err_${Prefix} \
           -c {params.threads} --mem={params.mem} -t {params.time}"
 
