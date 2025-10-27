02b000213d2da86856ed2fee74bf93aef80b0582
Alexandre <alexandre.laverre@unil.ch>
2025-10-27 18:52:33 +0100

    work on final figures

--- 3_analysis/statistics_analysis/SupFig_droso_sample_size.R
+++ 3_analysis/statistics_analysis/SupFig_droso_sample_size.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 path <- "/Users/alaverre/Documents/Detecting_positive_selection/cluster/results/positive_selection/NarrowPeaks/drosophila/modERN"
-pathFigures <- "/Users/alaverre/Documents/Detecting_positive_selection/results/figures/"
+pathFigures <- "/Users/alaverre/Documents/Detecting_positive_selection/cluster/results/final_figures/"
 sp = "drosophila"
 
 method="exact_ranked_ancestral" 
@@ -31,8 +31,10 @@ ggplot(df, aes(x = stage, y = sample_size, fill = stage)) +
   theme_minimal() +
   theme(
     legend.position = "none",              # remove legend
-    axis.text = element_text(size = 14),   # axis tick text
-    axis.title = element_text(size = 15)   # axis labels
+    axis.text = element_text(size = 11),   # axis tick text
+    axis.title = element_text(size = 11)   # axis labels
   ) +
   labs(title="N=674 experiments", y = "Number of Peaks", x = "")
-ggsave(paste0(pathFigures, "/figS1_droso_sample_size.pdf", width = 6, height = 4, useDingbats = FALSE))
+
+ggsave(paste0(pathFigures, "/SupFig_droso_sample_size.pdf"), width = 6, height = 4, useDingbats = FALSE)
+
